f7230c08e6ca0c0c9123d79976544d6172c1a219
kingsallen <lcfei1988@126.com>
2018-06-05 23:07:54 +0800

    initial commit

---
+++ src/com/lcf/idea/Demo.java
@@ -0,0 +1,12 @@
+package com.lcf.idea;
+
+import java.util.jar.Manifest;
+
+/**
+ * Created by Administrator on 2018/5/29.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("kkk");
+    }
+}
